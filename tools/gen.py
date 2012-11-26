32fad5ad0f9f906035b52618f3dec2ccdc3a7d8d
Jake Burton <jake5991@live.com>
2012-11-26 19:54:55 +0000

    Testing gen.py etc

--- tools/gen.py
+++ tools/gen.py
@@ -4,7 +4,9 @@ import re
 import platform
 import tempfile as temp
 import random
-#import markdown
+import markdown
+
+version = "v0.1"
 
 usageinfo =  \
 """
@@ -13,144 +15,126 @@ usageinfo =  \
 
 """
 
-class colour:
-	INFO = '\033[36m'
-	OK = '\033[32m'
-	WARN = '\033[33m'
-	END = '\033[0m'
-    
-	def disable(self):
-		self.INFO = ''
-		self.OK = ''
-		self.WARN = ''
-		self.END = ''
-
-colour = colour()
-
-try:
-	from colorama import init
-	init()
-except ImportError:
-	if (platform.system() == "Windows"):
-		colour.disable()
-
 class expression:
-	BOOK_INFO = re.compile('<book.+>')
-	LICENCE_INFO = re.compile('.*<licence .+/>')
-	COLOPHON = re.compile('.*<colophon.+>')
-	UNKNOWN = re.compile('.*<.+>.*')
-	ABOUT_DIR = re.compile('.*<about.+/>.*')
-	WORD = re.compile('.*<word>.+</word>.*')
-	FILE = re.compile('.*<file.+/>.*')
-	
+    BOOK_INFO = re.compile('<book.+>')
+    LICENCE_INFO = re.compile('.*<licence .+/>')
+    COLOPHON = re.compile('.*<colophon.+>')
+    UNKNOWN = re.compile('.*<.+>.*')
+    ABOUT_DIR = re.compile('.*<about.+/>.*')
+    WORD = re.compile('.*<word>.+</word>.*')
+    FILE = re.compile('.*<file.+/>.*')
+
 class config:
     path = ''
     aboutfiles = []
     metadata = {'title': '', 'colophon': '', 'extra': '', 'licence': '', 'licence-url': '', 'ver': ''}
 
-	
+
 def dictFromLine(line):
-	pairs = []
-	ValueString = re.findall('[a-z]*=".+"', line)
-	Seperate = re.split('"\s', ValueString[0])
-	for i in Seperate:
-		pairs.append(i.replace("\"","").split('='))
-	
-	return dict(pairs)
+    pairs = []
+    ValueString = re.findall('[a-z]*=".+"', line)
+    Seperate = re.split('"\s', ValueString[0])
+    for i in Seperate:
+        pairs.append(i.replace("\"","").split('='))
+
+    return dict(pairs)
 
 def main():
-    print "Checked path \t" + colour.OK + "OK" + colour.END
     try:
-    	prelims = open(config.path + "prelims.xml", 'r')
+        prelims = open(config.path + "prelims.xml", 'r')
     except:
-    	abort("File Error")
-    	
+        abort("File Error")
+    
+    print version + " Checked path \t [ OK ]"
+
     for line in prelims:
-    	if (expression.BOOK_INFO.match(line)):
-    		xml = dictFromLine(line)
-    		config.metadata['title'] = xml['title']
-    	elif (expression.LICENCE_INFO.match(line)):
-    		xml = dictFromLine(line)
-    		config.metadata['licence-url'] = xml['url']
-    		config.metadata['licence'] = xml['name']
-    	elif (expression.COLOPHON.match(line)):
-    		xml = dictFromLine(line)
-    		config.metadata['ver'] = xml['version']
-    	else:
-    		if not (expression.UNKNOWN.match(line)):
-    			config.metadata['colophon'] = config.metadata['colophon'] + line.lstrip()
-    	
+        if (expression.BOOK_INFO.match(line)):
+            xml = dictFromLine(line)
+            config.metadata['title'] = xml['title']
+        elif (expression.LICENCE_INFO.match(line)):
+            xml = dictFromLine(line)
+            config.metadata['licence-url'] = xml['url']
+            config.metadata['licence'] = xml['name']
+        elif (expression.COLOPHON.match(line)):
+            xml = dictFromLine(line)
+            config.metadata['ver'] = xml['version']
+        else:
+            if not (expression.UNKNOWN.match(line)):
+                config.metadata['colophon'] = config.metadata['colophon'] + line.lstrip()
+
     prelims.close()
-    
+
     for root, dirs, files in os.walk(config.path):
-    	for filename in files:
-       		if (filename.endswith('about.xml')):
-       			config.aboutfiles.append(root + "/" + filename)
-    
-    tmp = open('../out/' + config.metadata['title'] + '.html', 'w')
+        for filename in files:
+            if (filename.endswith('about.xml')):
+                config.aboutfiles.append(root + "/" + filename)
+	
+    tmp = open('../out/temp.tmp', 'w')
     for path in config.aboutfiles:
-    	file = open(path, 'r')
-    	keywords = []
-    	filelist = []
-    	topic = ''
-    	directory = os.path.dirname(path) + "/"
-    	for line in file:
-    		if (expression.ABOUT_DIR.match(line)):
-    			try:
-    				xml = dictFromLine(line)
-    				topic = xml['topic'].strip()
-    			except:
-    				abort("XML Error")
-    		elif (expression.WORD.match(line)):
-    			keywords.append(line.replace('<word>', '').replace('</word>', '').strip())
-    		elif (expression.FILE.match(line)):
-    			try:
-    				xml = dictFromLine(line)
-    				filelist.append(directory + xml['path'].strip())
-    			except:
-    				abort("XML Error")
-	#tmp.write(config)	
-	tmp.write('<hr>')
-	tmp.write('<h1>' + topic + '</h1>')
-	if keywords:
-		tmp.write('<h3> Keywords </h3>')
+        file = open(path, 'r')
+        keywords = []
+        filelist = []
+        topic = ''
+        directory = os.path.dirname(path) + "/"
+        tmp = open('../out/temp.tmp', 'a')
+        for line in file:
+            if (expression.ABOUT_DIR.match(line)):
+                try:
+                    xml = dictFromLine(line)
+                    topic = xml['topic'].strip()
+                except:
+                    abort("XML Error")
+            elif (expression.WORD.match(line)):
+                keywords.append(line.replace('<word>', '').replace('</word>', '').strip())
+            elif (expression.FILE.match(line)):
+                try:
+                    xml = dictFromLine(line)
+                    filelist.append(directory + xml['path'].strip())
+                except:
+                    abort("XML Error")
+        #tmp.write(config)
+
+			
+        if keywords:
+            tmp.write('<h3> Keywords </h3>')
+
+        for keyword in keywords:
+            tmp.write('<ul>')
+            tmp.write('<li>'+ keyword + '</li>')
+            tmp.write('</ul>')
+
+        for file in filelist:
+            tmp.write('<div id="' + os.path.basename(file).strip().replace('.md', '') + '">'  + '\n')
+            tmp.write('<h2>' + os.path.basename(file).strip().replace('.md', '').replace('_', ' ') + '</h2>'  + '\n')
+            read = open(file, 'r')
+            text = read.read()
+            tmp.write(markdown.markdown(text))
+
+	tmp.close()
+	content = open('../out/temp.tmp', 'r').read()
 
 	for keyword in keywords:
-		tmp.write('<ul>')
-		tmp.write('<li>'+ keyword + '</li>')
-		tmp.write('</ul>')
+		content = content.replace(keyword, '<strong>' + keyword + '</strong>')
+		
+	final = open('../out/' + config.metadata['title'] + '.html', 'w')
+	final.write(content)
+	final.close()
 
-    	for file in filelist:
-		tmp.write('<div id="' + os.path.basename(file).strip().replace('.md', '') + '">'  + '\n')
-    		tmp.write('<h2>' + os.path.basename(file).strip().replace('.md', '').replace('_', ' ') + '</h2>'  + '\n')
-    		read = open(file, 'r')
-    		for keyword in keywords:
-    			for line in read:
-    				if (keyword in line):
-    					tmp.write(line.replace(keyword, '<strong>' + keyword + '</strong>'))
-    				else:
-					tmp.write(line)
-		
-		read.close()
-	
-		
-	
-    					
+
 
 
 def isDir(path):
-	return os.path.isdir(path)
+    return os.path.isdir(path)
 
 def abort(message):
-	print colour.WARN + message + colour.END
-	sys.exit()
-	
+    print message
+    sys.exit()
+
 config = config()
 
 if (len(sys.argv) == 2):
-	if (isDir(sys.argv[1])):
-		config.path = sys.argv[1]
-		main()
+    if (isDir(sys.argv[1])):
+        config.path = sys.argv[1]
+        main()
 else:
-	print usageinfo
-
+    print usageinfo
